10e5b317d1b73ddfd891df40a29294db884cd5e8
rahul4693 <43261621+rahul4693@users.noreply.github.com>
2025-05-26 12:06:22 -0400

    Updated response message

--- Chapter02/kubia/app.js
+++ Chapter02/kubia/app.js
@@ -6,7 +6,7 @@ console.log("Kubia server starting...");
 var handler = function(request, response) {
   console.log("Received request from " + request.connection.remoteAddress);
   response.writeHead(200);
-  response.end("You've hit " + os.hostname() + "\n");
+  res.end(`You've hit the UPDATED instance on ${os.hostname()}\n`);
 };
 
 var www = http.createServer(handler);
